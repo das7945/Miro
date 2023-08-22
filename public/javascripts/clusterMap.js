ce96ec2eaf2baf21c76c5aeb946f6a618a2eb11b
das7945 <das7945@naver.com>
2023-08-23 02:29:32 +0900

    MapBox 기본위치 한국으로 변경

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,8 @@ const map = new mapboxgl.Map({
   container: "cluster-map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/light-v11",
-  center: [-103.5917, 40.6699],
+  // center: [-103.5917, 40.6699],
+  center: [126.978, 37.5665], // 기본위치 한국으로 변경
   zoom: 3,
 });
 
